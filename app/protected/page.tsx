9e23415adbb9cdba800dd6d29ae9b36cf5eb38e5
creatif-sam <samuel.creatiftech@gmail.com>
2026-01-11 17:04:06 +0100

    profile fix 4

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -2,7 +2,6 @@ import { redirect } from "next/navigation";
 
 import { createSupabaseServerClient } from "@/lib/supabase/server";
 import { InfoIcon } from "lucide-react";
-import { FetchDataSteps } from "@/components/tutorial/fetch-data-steps";
 import { Suspense } from "react";
 import Dashboard from "@/app/dashboard/layout";
 
