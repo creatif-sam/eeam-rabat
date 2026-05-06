14f09b38ae3085ab15a41ae67c6d0dd4a1468a73
creatifsam <samuel.creatiftech@gmail.com>
2026-05-06 16:12:43 +0100

    seperating entretiens pastoral for a page

--- supabase/migrations/20260506_set_all_users_to_membre_cp.sql
+++
@@ -1,6 +0,0 @@
--- Set all users to membre_cp except samuel.creatiftech@gmail.com
-UPDATE public.profiles
-SET role = 'membre_cp'
-WHERE id != (
-  SELECT id FROM auth.users WHERE email = 'samuel.creatiftech@gmail.com'
-);
