7c8925931b7d6689fcb3f31569353bf9cf3fea8f
creatifsam <samuel.creatiftech@gmail.com>
2026-06-18 20:44:54 +0100

    resolving issue of stale path on the entretiens pastoral page

--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -59,7 +59,8 @@ export async function updateSession(request: NextRequest) {
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/auth") &&
     !request.nextUrl.pathname.startsWith("/politique-de-confidentialite") &&
-    !request.nextUrl.pathname.startsWith("/politique-cookies")
+    !request.nextUrl.pathname.startsWith("/politique-cookies") &&
+    !request.nextUrl.pathname.startsWith("/entretien-pastoral")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
